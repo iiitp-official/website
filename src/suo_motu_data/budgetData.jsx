293d90204229d4bd17e86b9136a6bdb3e8f84999
Aanoush-Surana <aanoushsurana@gmail.com>
2026-06-23 08:36:49 +0530

    Added content in suo moto disclosure

--- src/suo_motu_data/budgetData.jsx
+++ src/suo_motu_data/budgetData.jsx
@@ -14,32 +14,47 @@ export const budgetData = [
   {
     sno: "2.1.1",
     item: "Total Budget for the public authority",
-    details: "Please click here to view the budget details (2024-25)",
-    link: "/assets/suo_motu/BUDGET%2024-25.pdf",
+    details: (
+      <a href="/assets/suo_motu/BUDGET%2024-25.pdf" target="_blank" rel="noopener noreferrer" className="link font-semibold">
+        Please click here to view the budget details (2024-25)
+      </a>
+    ),
   },
   {
     sno: "2.1.2",
     item: "Budget for each agency and plan & programmes",
-    details: "Please click here to view the budget details (2024-25)",
-    link: "/assets/suo_motu/BUDGET%2024-25.pdf",
+    details: (
+      <a href="/assets/suo_motu/BUDGET%2024-25.pdf" target="_blank" rel="noopener noreferrer" className="link font-semibold">
+        Please click here to view the budget details (2024-25)
+      </a>
+    ),
   },
   {
     sno: "2.1.3",
     item: "Proposed expenditures",
-    details: "Please click here to view the budget details (2024-25)",
-    link: "/assets/suo_motu/BUDGET%2024-25.pdf",
+    details: (
+      <a href="/assets/suo_motu/BUDGET%2024-25.pdf" target="_blank" rel="noopener noreferrer" className="link font-semibold">
+        Please click here to view the budget details (2024-25)
+      </a>
+    ),
   },
   {
     sno: "2.1.4",
     item: "Revised budget for each agency, if any",
-    details: "Please click here to view the budget details (2024-25)",
-    link: "/assets/suo_motu/BUDGET%2024-25.pdf",
+    details: (
+      <a href="/assets/suo_motu/BUDGET%2024-25.pdf" target="_blank" rel="noopener noreferrer" className="link font-semibold">
+        Please click here to view the budget details (2024-25)
+      </a>
+    ),
   },
   {
     sno: "2.1.5",
     item: "Report on disbursements made and place where the related reports are available",
-    details: "Please click here to view the budget details (2024-25)",
-    link: "/assets/suo_motu/BUDGET%2024-25.pdf",
+    details: (
+      <a href="/assets/suo_motu/BUDGET%2024-25.pdf" target="_blank" rel="noopener noreferrer" className="link font-semibold">
+        Please click here to view the budget details (2024-25)
+      </a>
+    ),
   },
   {
     sno: "2.2",
@@ -50,31 +65,37 @@ export const budgetData = [
   {
     sno: "2.2.1",
     item: "Budget",
-    details: "Please click here to view the travel details",
-    link: "/assets/suo_motu/2.2.1.pdf",
+    details: (
+      <a href="/assets/suo_motu/2.2.1.pdf" target="_blank" rel="noopener noreferrer" className="link font-semibold">
+        Please click here to view the travel details
+      </a>
+    ),
   },
   {
     sno: "2.2.2",
-    item: "Foreign and domestic Tours by ministries and officials of the rank of Joint Secretary to the Government and above...",
+    item: "Foreign and domestic Tours by ministries and officials of the rank of Joint Secretary to the Government and above, as well as the heads of the Department (a) Places visited (b) The period of visit (c) The number of members in the official delegation (d) Expenditure on the visit",
     details: "Not Applicable",
   },
   {
     sno: "2.2.3",
-    item: "Information related to procurements",
-    details: [
-      {
-        text: "Live E-Tender Details",
-        link: "/e-tender/live",
-      },
-      {
-        text: "Archive E-Tender Details",
-        link: "/assets/suo_motu/Awarde%20Tender.pdf",
-      },
-      {
-        text: "Awarded Tender Details",
-        link: "/assets/suo_motu/Awarde%20Tender.pdf",
-      },
-    ],
+    item: "Information related to procurements (a) Notice/tender enquires, and corrigenda if any thereon, (b) Details of the bids awarded comprising the names of the suppliers of goods/services being procured, (c) The works contracts concluded – in any such combination of the above-and, (d) The rate/ rates and the total amount at which such procurement or works contract is to be executed.",
+    details: (
+      <>
+        <a href="/e-tender/live" className="link font-semibold">
+          Please click here to view the Live E-Tender details.
+        </a>
+        <p className="mt-2">
+          <a href="/assets/suo_motu/Awarde%20Tender.pdf" target="_blank" rel="noopener noreferrer" className="link font-semibold">
+            Please click here to view the Archive E-Tender details.
+          </a>
+        </p>
+        <p className="mt-2">
+          <a href="/assets/suo_motu/Awarde%20Tender.pdf" target="_blank" rel="noopener noreferrer" className="link font-semibold">
+            Please click here to view the Awarded Tender details.
+          </a>
+        </p>
+      </>
+    ),
   },
   {
     sno: "2.3",
@@ -124,7 +145,7 @@ export const budgetData = [
   },
   {
     sno: "2.4",
-    item: "Discretionary and Non-discretionary Grants",
+    item: "Discretionary and Non-discretionary Grants [F. No. 1/6/2011-IR dt. 15.04.2013]",
     details: "",
     isHeading: true,
   },
@@ -136,30 +157,40 @@ export const budgetData = [
   {
     sno: "2.4.2",
     item: "Annual accounts of all legal entities who are provided grants by public authorities",
-    details: "Refer Annual Account Statement of (2024-25)",
-    link: "/assets/suo_motu/Balance%20Sheet%202024-25.pdf",
+    details: (
+      <>
+        Refer Annual Account Statement of (2024-25)
+        <br />
+        <a href="/assets/suo_motu/Balance%20Sheet%202024-25.pdf" target="_blank" rel="noopener noreferrer" className="link font-semibold">
+          Please click here to view the details.
+        </a>
+      </>
+    ),
   },
   {
     sno: "2.5",
-    item: "Particulars of Recipients of Concessions, Permits of Authorizations granted by the Public Authority",
+    item: "Particulars of Recipients of Concessions, Permits of Authorizations granted by the Public Authority [Section 4(1) (b) (xiii)]",
     details: "",
     isHeading: true,
   },
   {
     sno: "2.5.1",
-    item: "Particulars of Recipients of Concessions, Permits of Authorizations granted by the Public Authority",
+    item: "Particulars of Recipients of Concessions, Permits of Authorizations granted by the Public Authority [Section 4(1) (b) (xiii)]",
     details: "Nil",
   },
   {
     sno: "2.6",
-    item: "CAG & PAC Paras",
+    item: "CAG & PAC Paras [F. No. 1/6/2011- IR dt. 15.4.2013]",
     details: "",
     isHeading: true,
   },
   {
     sno: "2.6.1",
-    item: "CAG & PAC PARAS",
-    details: "The CAG audit for FY 2024-25",
-    link: "/assets/suo_motu/CAG%20audit%20report%20FY_2024-25.pdf",
+    item: "CAG & PAC PARAS [F NO. 1/6/2011- IR DT.15.4.2013]",
+    details: (
+      <a href="/assets/suo_motu/CAG%20audit%20report%20FY_2024-25.pdf" target="_blank" rel="noopener noreferrer" className="link font-semibold">
+        The CAG audit for FY 2024-25
+      </a>
+    ),
   },
 ];
